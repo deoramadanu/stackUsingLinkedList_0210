5043502bccf9ce81c75921abdf5c51f47ca00866
deoramadanu <deo121005@gmail.com>
2025-05-20 10:26:44 +0700

    bool isEmpty

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -54,5 +54,21 @@ class Stack
             {
                 cout << "List is Empty ." << endl;
             }
+            else
+            {
+                Node *current = top;
+                while (current != NULL)
+                {
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+            }
+        }
+
+        bool isEmpty()
+        {
+            return top == NULL;
         }
 };
+
