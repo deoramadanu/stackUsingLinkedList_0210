e828bfb4702f5beaa2b108d12826e85e275360a6
deoramadanu <deo121005@gmail.com>
2025-05-20 10:12:30 +0700

    menampilkan value

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -29,6 +29,10 @@ class Stack
 
         int push(int value)
         {
-
-        }
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            
 };
